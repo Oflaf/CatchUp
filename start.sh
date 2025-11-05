d0e4526e7f321963377b881455dcc166a54b6aec
Oflaf <olo.marcinkowski@gmail.com>
2025-11-05 23:17:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e
 
-# Uruchom główny serwer gry w tle. 
-# On automatycznie użyje portu $PORT (10000)
-echo "Starting Game Server..."
+echo "Starting Game Server in background..."
 npm start &
 
-# Uruchom serwer PeerJS na INNYM, wewnętrznym porcie, np. 9000
-echo "Starting PeerJS Server..."
-npx peerjs --port 9000 --path /peerjs
+echo "Starting PeerJS Server in foreground..."
+npx peerjs --port 10000 --path /peerjs
 
-# Czekaj na zakończenie procesu PeerJS
 wait -n
